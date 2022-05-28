c728010e1b497084156a61d75a21d8540b821874
hisahito <hisahito706@gmail.com>
2022-05-27 23:16:30 -0500

    adding method valorCarta() to have the value of each card

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -42,6 +42,8 @@ const pedirCarta = () => {
 }
 
 const valorCarta = (carta) => {
-
-
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === "A" ) ? 11 : 10
+            : valor * 1;
 }
