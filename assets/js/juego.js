6aa7e65acf9dc4d0b8beda1941b0c7a1aa601f02
hisahito <hisahito706@gmail.com>
2022-05-27 23:02:04 -0500

    adding underscorejs to use the shiffle method in my deck

---
+++ assets/js/juego.js
@@ -0,0 +1,47 @@
+/**
+ * * 2C = TWO OF CLUBS
+ * * 2D = TWO OF DIAMONDS
+ * * 2H = TWO OF HEARTS
+ * * 2S = TWO OF SPADES
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['J', 'Q', 'K', 'A'];
+
+// Esta función crea una baraja de cartas
+const crearDeck = () => {
+
+    for (i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    } 
+
+    for(let tipo of tipos) {
+        for(let especial of especiales) {
+            deck.push(especial + tipo);
+        }
+    }
+
+    deck = _.shuffle( deck );
+    console.log(deck);
+    return deck;
+}
+
+crearDeck();
+
+// Esta funcion me permite tomar una carta
+
+const pedirCarta = () => {
+    if( deck.length = 0){
+        throw new Error('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+const valorCarta = (carta) => {
+
+
+}
